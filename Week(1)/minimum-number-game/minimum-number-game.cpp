f0ace13d4ddc31acfb88223891441646826976d7
youssefesmail6 <youssefmohamedesmail@yahoo.com>
2025-12-01 23:15:37 +0200

    feat:solved min nnumber game

---
+++ Week(1)/minimum-number-game/minimum-number-game.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+      sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i=i+2)
+        {
+            swap(nums[i],nums[i+1]);
+        }
+        return nums;
+    }
+};
